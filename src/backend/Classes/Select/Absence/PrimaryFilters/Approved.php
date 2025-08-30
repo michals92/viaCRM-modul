cffd96dce2ae19f8b30fc7a54c1b7140ccf0f14f
Martin <martinpeterka4423@gmail.com>
2025-08-31 00:26:49 +0200

    icon + primary filter fix

---
+++ src/backend/Classes/Select/Absence/PrimaryFilters/Approved.php
@@ -0,0 +1,19 @@
+<?php
+namespace Espo\Modules\ViaCrm\Classes\Select\Absence\PrimaryFilters;
+
+use Espo\Core\Select\Primary\Filter;
+use Espo\ORM\Query\SelectBuilder;
+use Espo\ORM\Query\Part\Condition as Cond;
+
+class Approved implements Filter
+{
+    public function apply(SelectBuilder $queryBuilder): void
+    {
+        $queryBuilder->where(
+            Cond::equal(
+                Cond::column('status'),
+                'approved'
+            )
+        );
+    }
+}
---
+++ src/backend/Classes/Select/Absence/PrimaryFilters/Cancelled.php
@@ -0,0 +1,19 @@
+<?php
+namespace Espo\Modules\ViaCrm\Classes\Select\Absence\PrimaryFilters;
+
+use Espo\Core\Select\Primary\Filter;
+use Espo\ORM\Query\SelectBuilder;
+use Espo\ORM\Query\Part\Condition as Cond;
+
+class Cancelled implements Filter
+{
+    public function apply(SelectBuilder $queryBuilder): void
+    {
+        $queryBuilder->where(
+            Cond::equal(
+                Cond::column('status'),
+                'cancelled'
+            )
+        );
+    }
+}
