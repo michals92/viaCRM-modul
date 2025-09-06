30fdc6bc23e62f5a55b3a8b0b4811e7757fe93ae
Martin <martinpeterka4423@gmail.com>
2025-09-06 22:02:16 +0200

    structure for Orders and Reports Entities

---
+++ src/backend/Controllers/Report.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Espo\Modules\ViaCrm\Controllers;
+
+use Espo\Core\Controllers\Record;
+
+class Report extends Record
+{
+}
---
+++ src/backend/Entities/Order.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Espo\Modules\ViaCrm\Entities;
+
+use Espo\Core\Templates\Entities\BasePlus;
+
+class Order extends BasePlus
+{
+    public const ENTITY_TYPE = 'Order';
+    
+}
