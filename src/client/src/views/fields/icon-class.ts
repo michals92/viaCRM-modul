cd87a8ae22114c348a86770bff4377b5ddd8fb3e
sovicka <stepan.frais@apertia.cz>
2025-11-26 09:32:41 +0100

    form

--- src/client/src/views/fields/icon-class.ts
+++ src/client/src/views/fields/icon-class.ts
@@ -1,4 +1,4 @@
 define(['views/admin/entity-manager/fields/icon-class'], Dep => class extends Dep {
-	override detailTemplate = 'autocrm:fields/icon-class/detail';
-	override listTemplate = 'autocrm:fields/icon-class/list';
+	override detailTemplate = 'viacrm:fields/icon-class/detail';
+	override listTemplate = 'viacrm:fields/icon-class/list';
 });
